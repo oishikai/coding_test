88a799cd154b3744af82ed26140b443449f4f7d5
Kai Oishi <bokuchan.kai@gmail.com>
2023-09-10 12:38:57 +0900

    first commit

---
+++ bin/coding_test.dart
@@ -0,0 +1,5 @@
+import 'package:coding_test/coding_test.dart' as coding_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${coding_test.calculate()}!');
+}
---
+++ test/coding_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:coding_test/coding_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
