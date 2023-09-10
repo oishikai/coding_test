b2b307b49c15cc46a472989011e9b5b0803bc86d
Kai Oishi <bokuchan.kai@gmail.com>
2023-09-10 12:46:00 +0900

    1問目 解答

--- bin/coding_test.dart
+++ bin/coding_test.dart
@@ -1,5 +1,29 @@
-import 'package:coding_test/coding_test.dart' as coding_test;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${coding_test.calculate()}!');
+void main() {
+  final a = stdin.readLineSync()!;
+
+  try {
+    final min = calculateMinRolls(a);
+    print(min);
+  } catch (e) {
+    print(e);
+  }
+}
+
+int calculateMinRolls(String A) {
+  // 数字以外の文字が含まれている場合
+  if (int.tryParse(A) == null) throw ArgumentError('数字以外の文字が含まれています');
+  int a = int.parse(A);
+
+  // 制約外の場合
+  if (a < 2 || 100000 < a) throw ArgumentError('2以上100000以下の整数を入力してください');
+
+  // 初期位置を考慮して-1
+  a = a - 1;
+
+  if (a % 6 == 0) {
+    return a ~/ 6;
+  }
+  return (a ~/ 6) + 1;
 }
